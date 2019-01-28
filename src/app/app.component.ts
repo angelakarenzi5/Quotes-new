a6ad7007a9778c5584e26e8f2a1c8032721303cf
Angela Uwase <angelakarenzi5@gmail.com>
2019-01-28 09:23:04 +0200

    components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,6 @@ export class AppComponent {
   Quotes: string[];
 
   constructor() {
-    this.Quotes = ["Create new quote", "Upvote quote", "Downvote quote"];
+    this.Quotes = ["Create new quote","Author of quote", "Upvote quote", "Downvote quote", "Number of upvotes and number of downvotes"];
   }
 }
