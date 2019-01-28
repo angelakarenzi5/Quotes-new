231c7b35ebd494fba581d0f0bd3903a8dd01312c
Angela Uwase <angelakarenzi5@gmail.com>
2019-01-28 12:33:01 +0200

    pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,13 @@ import {Quote} from './quote'
 })
 export class AppComponent {
     quotes = [
-      new Quote(1,'Create new quote', 'Write down your quote here'),
-      new Quote(2,'Author of quote', 'Write the name of the person who wrote quote'),
-      new Quote(3, 'Upvote quote', 'This upvote quote selection shows you liked the quote'),
-      new Quote(4, 'Downvote quote', 'This downvote quote selection shows you disliked the quote '),
-      new Quote(5, 'Number of Upvotes', 'This is the total number of upvotes for this quote '),
-      new Quote(6, 'Number of Downvotes', 'This shows the total number of downvotes for this quote'),
+      new Quote(1, "Create new quote", "Write down your quote here", new Date(2019,3,14)),
+      new Quote(2,"Author of quote","Write the name of the person who wrote quote", new Date(2019,6,9)),
+      new Quote(3,"Upvote quote","This upvote quote selection shows you liked the quote", new Date(2019,1,12)),
+      new Quote(4, "Downvote quote", "This downvote quote selection shows you disliked the quote ", new Date(2019,1,12)),
+      new Quote( 5,"Number of Upvotes","This is the total number of upvotes for this quote ",new Date(2019,5,18)),
+      new Quote( 6, "Number of Downvotes", "This shows the total number of downvotes for this quote",new Date(2019,7,14))
+    ];
+  
 
-    ]
 }
