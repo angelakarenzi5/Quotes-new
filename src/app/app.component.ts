593086999941c7edd479be7d2b53dc5ecd7d4073
Angela Uwase <angelakarenzi5@gmail.com>
2019-01-28 09:17:38 +0200

    first steps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,12 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  templateUrl: "./app.component.html"
 })
 export class AppComponent {
-  constructor(){}
+  Quotes: string[];
+
+  constructor() {
+    this.Quotes = ["Create new quote", "Upvote quote", "Downvote quote"];
+  }
 }
